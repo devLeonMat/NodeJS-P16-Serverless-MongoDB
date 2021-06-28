d65270487b6504ba1271b2308d7c69388967f388
leon matias <leon.matias.dev@gmail.com>
2021-06-28 08:06:32 -0500

    add changes

--- handler.js
+++ handler.js
@@ -6,21 +6,96 @@ const express = require('express');
 const app = express();
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
+const ACCOUNT_TABLE = process.env.ACCOUNT_TABLE;
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDB;
+
+if (IS_OFFLINE) {
+    dynamoDB = new AWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:3000'
+    });
+} else {
+    dynamoDB = new AWS.DynamoDB.DocumentClient();
+}
+
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.post('/accounts', (req, res)=>{
+app.post('/accounts', (req, res) => {
     const {accountId, name} = req.body;
-    res.json({accountId, name})
+
+    const params = {
+        TableName: ACCOUNT_TABLE,
+        Item: {
+            accountId, name
+        }
+    };
+    dynamoDB.put(params, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(400).json({
+                error: 'Error creando cuenta'
+            })
+        } else {
+            res.json({accountId, name})
+        }
+    });
 });
+
+app.get('/accounts', (req, res) => {
+    const params = {
+        TableName: ACCOUNT_TABLE,
+    };
+    dynamoDB.scan(params, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(400).json({
+                error: 'Error obteniendo cuentas'
+            })
+        } else {
+            const {Items} = result;
+            res.json({
+                success: true,
+                message: 'Cuentas obtenidas',
+                accounts: Items
+            })
+        }
+    });
+
+});
+
+app.get('/accounts/:accountId', (req, res) => {
+    const params = {
+        TableName: ACCOUNT_TABLE,
+        Key: {
+            accountId: req.params.accountId
+        }
+    };
+    dynamoDB.get(params, (err, result) => {
+        if (err) {
+            res.status(400).json({
+                error: 'we cant access to account'
+            })
+        }
+        if (result.Item) {
+            const {accountId, name} = result.Item
+            return res.json({
+                accountId, name
+            });
+        } else {
+            res.status(400).json({
+                error: 'Account not found!'
+            })
+        }
+    });
+})
 
 
 module.exports.generic = serverless(app);
